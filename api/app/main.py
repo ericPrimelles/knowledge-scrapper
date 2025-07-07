bb7dc487c4b226f57bb9c86033d1b2367cc6735f
eric <eric.a.primelles96@gmail.com>
2025-07-07 12:18:17 -0400

    Deploying scrapper

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from scrapper import scrapper_routine
 from pydantic import BaseModel
 from typing import List
@@ -10,3 +10,7 @@ class ScrapperBody(BaseModel):
 @app.post("/scrapper")
 async def scrapper(body:ScrapperBody):
     return await scrapper_routine(dict(body), {})
+
+@app.get("/health")
+async def health():
+    return Response(status_code=200)
